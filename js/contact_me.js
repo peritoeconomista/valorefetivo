34e2372c46b8433b420ea86bd91b454eaea470e1
Luan Fonseca <luanfonceca@gmail.com>
2018-05-30 02:59:37 -0300

    Integrate mailchimp and valorefetivo form.

--- js/contact_me.js
+++ js/contact_me.js
@@ -13,31 +13,38 @@ $(function() {
       var phone = $("input#phone").val();
       var message = $("textarea#message").val();
       var firstName = name; // For Success/Failure Message
+      var lastName = '';
       // Check for white space in name for Success/Fail message
       if (firstName.indexOf(' ') >= 0) {
-        firstName = name.split(' ').slice(0, -1).join(' ');
+        firstName = name.split(' ')[0];
+        lastName = name.split(' ').slice(1).join(' ');
       }
       $this = $("#sendMessageButton");
       $this.prop("disabled", true); // Disable submit button until AJAX call is complete to prevent duplicate messages
       $.ajax({
-        url: "././mail/contact_me.php",
-        type: "POST",
+        type: $form.attr('method'),
+        url: $form.attr('action'),
         data: {
-          name: name,
-          phone: phone,
-          email: email,
-          message: message
+          EMAIL: email,
+          FNAME: firstName,
+          LNAME: lastName,
+          PHONE: phone,
+          MESSAGE: message,
         },
         cache: false,
-        success: function() {
-          // Success message
-          $('#success').html("<div class='alert alert-success'>");
-          $('#success > .alert-success').html("<button type='button' class='close' data-dismiss='alert' aria-hidden='true'>&times;")
-            .append("</button>");
-          $('#success > .alert-success')
-            .append("<strong>Your message has been sent. </strong>");
-          $('#success > .alert-success')
-            .append('</div>');
+        dataType: 'json',
+        contentType: "application/json; charset=utf-8",
+        success: function(response) {
+          if (response.result == 'success') {
+            // Success message
+            $('#success').html("<div class='alert alert-success'>");
+            $('#success > .alert-success').html("<button type='button' class='close' data-dismiss='alert' aria-hidden='true'>&times;")
+              .append("</button>");
+            $('#success > .alert-success')
+              .append("<strong>Sua mensagem foi entregue com sucesso. </strong>");
+            $('#success > .alert-success')
+              .append('</div>');
+          }
           //clear all fields
           $('#contactForm').trigger("reset");
         },
